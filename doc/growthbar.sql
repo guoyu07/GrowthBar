6e983f448280eb1db38ddc1f5ea9f1bbf7240e45
张健顺 <1224522500@qq.com>
2017-08-19 15:30:45 +0800

    更新数据库文件

--- doc/growthbar.sql
+++ doc/growthbar.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2017-07-28 11:31:48
+Date: 2017-08-19 15:29:54
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,6 +25,7 @@ CREATE TABLE `activity` (
   `activity_place` varchar(255) DEFAULT NULL COMMENT '活动地点',
   `activity_time` int(11) NOT NULL COMMENT '活动时间',
   `status` int(2) NOT NULL COMMENT '活动状态',
+  `activity_compere` varchar(255) DEFAULT NULL COMMENT '活动主讲人、主持人',
   PRIMARY KEY (`activity_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -40,9 +41,10 @@ CREATE TABLE `appointment` (
   `appointment_id` int(11) NOT NULL COMMENT '预约编号',
   `activity_id` int(1) NOT NULL COMMENT '活动编号',
   `user_account` varchar(20) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL COMMENT '用户账号',
-  `user_num` int(13) NOT NULL COMMENT '人数',
+  `description` text NOT NULL COMMENT '备注',
   `user_tel` char(11) NOT NULL COMMENT '联系方式',
   `status` int(2) NOT NULL COMMENT '预约状态',
+  `time` int(11) NOT NULL COMMENT '预约时间',
   PRIMARY KEY (`appointment_id`),
   KEY `user_appointment_tel` (`user_tel`),
   KEY `user_activity_id` (`activity_id`),
@@ -66,7 +68,7 @@ CREATE TABLE `article` (
   `post_time` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '发布时间',
   `status` int(2) NOT NULL COMMENT '文章状态',
   PRIMARY KEY (`article_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of article
@@ -84,6 +86,7 @@ CREATE TABLE `comment` (
   `comment_content` varchar(50) NOT NULL COMMENT '评论内容',
   `comment_time` int(11) NOT NULL COMMENT '评论时间',
   `status` int(2) NOT NULL COMMENT '评论状态',
+  `parent_id` int(11) NOT NULL COMMENT '父评论编号',
   PRIMARY KEY (`comment_id`),
   KEY `user_postbar_id` (`user_account`),
   CONSTRAINT `user_postbar_id` FOREIGN KEY (`user_account`) REFERENCES `user_information` (`user_account`) ON DELETE NO ACTION ON UPDATE CASCADE
@@ -103,6 +106,7 @@ CREATE TABLE `post` (
   `post_content` varchar(255) NOT NULL COMMENT '帖子内容',
   `post_time` int(11) NOT NULL COMMENT '发帖时间',
   `user_account` varchar(255) DEFAULT '' COMMENT '发帖人',
+  `post_status` int(11) DEFAULT NULL COMMENT '帖子状态',
   PRIMARY KEY (`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -155,7 +159,7 @@ CREATE TABLE `teacher` (
   `teacher_email` varchar(15) NOT NULL COMMENT '教师邮箱',
   `teacher_desc` varchar(50) DEFAULT NULL COMMENT '教师介绍',
   PRIMARY KEY (`teacher_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of teacher
@@ -173,6 +177,7 @@ CREATE TABLE `user_information` (
   `user_sex` int(1) NOT NULL,
   `user_description` varchar(100) DEFAULT NULL,
   `user_truename` varchar(20) NOT NULL,
+  `user_status` int(11) NOT NULL COMMENT '用户状态',
   PRIMARY KEY (`user_id`),
   KEY `user_phone` (`user_phone`),
   KEY `user_id` (`user_account`),
