8f7974482dba83b9ecb9e952b553a2e57f547afc
张世俊 <864953634@qq.com>
2017-09-17 15:42:40 +0800

    匿名贴吧模块前端接口完善

--- WebRoot/js/AnoEdit.js
+++ WebRoot/js/AnoEdit.js
@@ -1,41 +1,64 @@
 $(document).on("click", "#submitTopic", function() {
-    var anoTopic = $("#anoTopic").val()
-    var userId = $("#userId").val()
     $.ajax({
-        url: '',
-        type: 'post',
+        url: '/User/check',
+        type: 'POST',
         dataType: 'json',
-        data: {
-            user_id: userId,
-            ano_name: anoTopic
-        },
         success: function(data) {
-        	if (data.status==true) {
-			window.location.href = 'Ano_index.html';
-        	}
-            else{
-            	alert("发帖失败，请重新发帖。");
+            if (data.status == true) {
+                var anoTopic = $("#anoTopic").val()
+                var anoContent = $("#anoContent").val()
+                $.ajax({
+                    url: '/postbar/add',
+                    type: 'post',
+                    dataType: 'json',
+                    data: {
+                        postTitle: anoTopic,
+                        postContent: anoContent,
+                    },
+                    success: function(data) {
+                        if (data.status == true) {
+                            window.location.href = 'Ano_index.html';
+                        } else {
+                            alert("发帖失败，请重新发帖。");
+                        }
+                    },
+                    error: function(XMLHttpRequest, textStatus, errorThrown) {
+                        alert(XMLHttpRequest.responseText);
+                        alert(XMLHttpRequest.readyState);
+                        alert(textStatus);
+                    },
+                })
+            } else {
+                alert("请登录");
             }
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
-            alert(XMLHttpRequest.responseText);
-            alert(XMLHttpRequest.readyState);
-            alert(textStatus);
         },
     })
 })
-
 $(document).ready(function() {
     $.ajax({
-        url: '',
+        url: '/User/check',
+        type: 'POST',
+        dataType: 'json',
+        success: function(data) {
+            if (data.status == true) {
+                var userAccount = data.userAccount;
+                $("#getUserID").html('<li><a>' + userAccount + '</a></li>');
+                $("#getUserID").toggle();
+            } else {
+                $("#getUserID").toggle();
+            }
+        },
+    })
+    $.ajax({
+        url: '/postbar/viewAll',
         type: 'post',
         dataType: 'json',
         success: function(data) {
-            var str = ''
-            $.each(data.ano, function(i, item) {
-                str += ('<li class="list-group-item"><a href="#">' + item.ano_name + '</a></li>')
+            var str = "";
+            $.each(data.postList, function(i, item) {
+                str += ('<li class="list-group-item"><a class="pageGetName" href="#" id="getName' + i + '">' + item.post_title + '<p id="getId' + i + '" hidden>' + item.post_id + '</p></a></li>');
             });
-            $("#addAnoTopic").html(str)
+            $("#anoTitle").html(str);
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
             alert(XMLHttpRequest.responseText);
--- WebRoot/js/AnoProcess.js
+++ WebRoot/js/AnoProcess.js
@@ -1,12 +1,26 @@
 $(document).ready(function($) {
     $.ajax({
-        url: '',
+        url: '/User/check',
+        type: 'POST',
+        dataType: 'json',
+        success: function(data) {
+            if (data.status == true) {
+                var userAccount = data.userAccount;
+                $("#getUserID").html('<li><a>' + userAccount + '</a></li>' + '<li><a href="Ano_edit.html" id="postMeg">发帖</a></li>');
+                $("#getUserID").toggle();
+            } else {
+                $("#getUserID").toggle();
+            }
+        },
+    });
+    $.ajax({
+        url: '/postbar/viewAll',
         type: "POST",
         dataType: "json",
         success: function(data) {
             var str = "";
-            $.each(data.postbars, function(i, item) {
-                str += ('<li class="list-group-item"><a class="pageGetName" href="#" id="getName' + i + '">' + item.postbar_title + '<p id="getId' + i + '" hidden>' + item.post_id + '</p></a></li>');
+            $.each(data.postList, function(i, item) {
+                str += ('<li class="list-group-item"><a class="pageGetName" href="#" id="getName' + i + '">' + item.post_title + '<p id="getId' + i + '" hidden>' + item.post_id + '</p></a></li>');
             });
             $("#anoTitle").html(str)
         },
@@ -21,18 +35,18 @@ $(function($) {
     $(".pageAll").click(function() {
         var page = $(this).html();
         $.ajax({
-            url: '',
+            url: '/postbar/viewAll',
             type: "POST",
             dataType: "json",
             data: {
-                pageNum: page
+                pageNum: page,
             },
             success: function(data) {
                 var str = "";
-                $.each(data.postbars, function(i, item) {
-                    str += ('<li class="list-group-item"><a class="pageGetName" href="#" id="getName' + i + '">' + item.postbar_title + '<p id="getId' + i + '" hidden>' + item.postbar_id + '</p></a></li>');
+                $.each(data.postList, function(i, item) {
+                    str += ('<li class="list-group-item"><a class="pageGetName" href="#" id="getName' + i + '">' + item.post_title + '<p id="getId' + i + '" hidden>' + item.post_id + '</p></a></li>');
                 });
-                $("#anoTitle").html(str)
+                $("#anoTitle").html(str);
             },
             error: function(XMLHttpRequest, textStatus, errorThrown) {
                 alert(XMLHttpRequest.responseText);
@@ -44,11 +58,11 @@ $(function($) {
 })
 $(document).on("click", ".pageGetName", function() {
     var textContent = $(this).clone();
-    textContent.find(':nth-child(n)').remove()
-    var topicTitle = textContent.html()
-    var ano_id = $(this).children('p').html()
+    textContent.find(':nth-child(n)').remove();
+    var topicTitle = textContent.html();
+    var ano_id = $(this).children('p').html();
     $.ajax({
-        url: '',
+        url: '/postbar-comment/viewCommentByPost',
         type: 'POST',
         dataType: 'json',
         data: {
@@ -62,11 +76,11 @@ $(document).on("click", ".pageGetName", function() {
             var str2 = ('</ul></div></div></div><div class="col-sm-2 col-md-2"></div></div>')
             var str = ('<div class="row Czb_margin_padding"><div class="col-sm-2 col-md-2"></div><div class="col-sm-8 col-md-8" ><div class="thumbnail"><div class="caption"><ul class="list-group" >')
             var content = ''
-            $.each(data.postbar_comment, function(i, item) {
-                content += ('<li class="list-group-item">' + item.comment_content + '<h2 class="text-right"><small>匿名用户'+item.comment_time+'</small></h2><p hidden>'+item.user_id+'</p>' + '</li>' + '</li>')
+            $.each(data.comments, function(i, item) {
+                content += ('<li class="list-group-item">' + item.comment_content + '<h2 class="text-right"><small>匿名用户' + item.comment_time + '</small></h2><p hidden>' + item.user_account + '</p>' + '</li>' + '</li>')
             });
-            var str4 = ('<div id="remove4"><div class="col-sm-2 col-xs-2"></div><div class="col-sm-8 col-xs-8">添加评论<textarea class="form-control commentContent" rows="10"></textarea></div><div class="col-sm-2 col-xs-2"></div></div>');
-            var button=('<div class="col-sm-offset-6 col-sm-6 Czb_Ano_blank" id="remove5"><button  class="btn btn-default " id="clickButton">提交</button></div>');
+            var str4 = ('<div id="remove4"><div class="col-sm-2 col-xs-2"></div><div class="col-sm-8 col-xs-8">添加评论<textarea class="form-control commentContent" rows="10" id="anoContent"></textarea></div><div class="col-sm-2 col-xs-2"></div></div>');
+            var button = ('<div class="col-sm-offset-6 col-sm-6 Czb_Ano_blank" id="remove5"><button  class="btn btn-default " id="commentButton">提交</button></div>');
             $('#navBlank').html(title);
             $("#testBlank").html(str + content + str2 + str4 + button);
         },
@@ -75,5 +89,34 @@ $(document).on("click", ".pageGetName", function() {
             alert(XMLHttpRequest.readyState);
             alert(textStatus);
         },
-    })
+    });
+    $(document).on('click','#commentButton',function(){
+        $.ajax({
+        url: '/User/check',
+        type: 'POST',
+        dataType: 'json',
+        success: function(data) {
+            if (data.status == true) {
+                var commentContent=$("#anoContent").val();
+                $.ajax({
+                    url: '/postbar-comment/comment',
+                    type: 'POST',
+                    dataType: 'json',
+                    data: {post_id:ano_id,content:commentContent},
+                    success:function(data){
+                        if (data.status==true) {
+                            alert("评论成功");
+                            window.location.href="Ano_index.html";
+                        }
+                        else{
+                            alert("请重新评论");
+                        }
+                    }
+                });
+            } else {
+                alert("请登录！");
+            }
+        },
+    });
+})
 })
